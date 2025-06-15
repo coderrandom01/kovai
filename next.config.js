9b03effe0b26cadfb3736597afd5ea2417242ffa
coderrandom01 <coderrandom01@gmail.com>
2025-06-15 21:50:38 +0530

    Image issue and tailwind issue

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['kovai-guppies-images.s3.eu-north-1.amazonaws.com'],
+    domains: ['kovai-guppies-images.s3.eu-north-1.amazonaws.com',"jprcwo3zvr1gqikh.public.blob.vercel-storage.com"],
   },
 };
 
