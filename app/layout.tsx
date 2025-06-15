1629816bee233fedb25d71f9bc09fb55424fed54
coderrandom01 <coderrandom01@gmail.com>
2025-06-15 23:29:28 +0530

    Favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head';
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
@@ -19,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+        <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={roboto.className}>{children}</body>
     </html>
   )
